64aa82651c395b1a34be61e5c073af853d98d2d5
pavin <pavin227@gmail.com>
2023-12-15 21:14:14 +0530

    First structure

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // You can add code here to handle the form submission, e.g., sending data to a server.
+    alert('Form submitted!');
+});
