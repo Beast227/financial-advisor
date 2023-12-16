db2d1780116ebd75c3d6ff9984cfb8c4a1ca6b5f
pavin <pavin227@gmail.com>
2023-12-16 23:11:09 +0530

    Account website added

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    var navList = document.querySelector('.nav-list');
+    navList.classList.toggle('show');
+});
+
 document.getElementById('contactForm').addEventListener('submit', function (event) {
     event.preventDefault();
     // You can add code here to handle the form submission, e.g., sending data to a server.
