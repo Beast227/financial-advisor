4acd3244172ee7d172b2eb8d9c1a1c1cbba5ac00
Varun Raj <varunraj0609@gmail.com>
2023-12-18 22:53:50 +0530

    Added login system and Css changes

--- Login web/login.js
+++ Login web/login.js
@@ -1,38 +1,47 @@
+// Function to handle login form submission
 function login() {
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
 
-  // Prepare data to send to the server
-  var data = {
-    username: username,
-    password: password
-  };
+  // Client-side validation
+  if (!validateLoginForm(username, password)) {
+      alert('Please enter valid credentials.');
+      return;
+  }
 
-  // Perform an AJAX request using the Fetch API
-  fetch('/your-backend-endpoint', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(responseData => {
-    // Handle the response from the server
-    if (responseData.success) {
-      alert('Login successful! Redirecting to dashboard.');
-       // Redirect to another HTML page
-       window.location.href = './index.html';
-    } else {
-      alert('Login failed. Please check your credentials and try again.');
-    }
-  })
-  .catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
-  });
+  // Simulate login logic (replace with actual server-side logic)
+  alert('Login successful! Redirecting to Home page.');
+  // Redirect to another HTML page
+  window.location.href = '../Main web/index.html';
+}
+
+// Function to handle signup form submission
+function signup() {
+  var newUsername = document.getElementById('newUsername').value;
+  var newPassword = document.getElementById('newPassword').value;
+
+  // Client-side validation
+  if (!validateSignupForm(newUsername, newPassword)) {
+      alert('Please enter valid signup information.');
+      return;
+  }
+
+  // Simulate signup logic (replace with actual server-side logic)
+  alert('Signup successful! Redirecting to Home page.');
+  // Redirect to the home page
+  window.location.href = '../Main web/index.html';
+}
+
+// Function to validate login form inputs
+function validateLoginForm(username, password) {
+  // Add your client-side validation logic here
+  // For example, check if username and password meet certain criteria
+  return username.length > 0 && password.length > 0;
+}
+
+// Function to validate signup form inputs
+function validateSignupForm(newUsername, newPassword) {
+  // Add your client-side validation logic here
+  // For example, check if newUsername and newPassword meet certain criteria
+  return newUsername.length > 0 && newPassword.length > 0;
 }
